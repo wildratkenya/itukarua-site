d7de68e65f61ad71164d93541fc29e8cbd28e129
Samuel Wambaa <itukarua2020@gmail.com>
2026-04-02 13:08:01 +0300

    connect to my supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Initialize database client
-const supabaseUrl = 'https://yxyqetgwueqxazojujta.databasepad.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImQyMjNmZDViLTdmM2EtNDNlZS05ZTlkLTYyYjJiMmFhYzk3NiJ9.eyJwcm9qZWN0SWQiOiJ5eHlxZXRnd3VlcXhhem9qdWp0YSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzcxMDU1Mzg4LCJleHAiOjIwODY0MTUzODgsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.zqBEFVLDlfGGOYMJGHIIodcBU0UnBK-kjTyW4zPiMpQ';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-export { supabase };
+export const supabase = createClient(supabaseUrl, supabaseKey)
